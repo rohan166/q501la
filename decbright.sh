426d55dcb62c67ff4cd12d4c75bffde9d0ea107f
Rohan Rangray <rohan166@gmail.com>
2013-10-09 16:26:52 -0700

    Update decbright.sh

--- decbright.sh
+++ decbright.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-BRIGHT=$(cat /sys/class/backlight/intel_backlight/brightness)
-MAXBRIGHT=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+# Make the directory containing $MAXBRIGHT and $BRIGHT your working
+# directory
+
+# brightness contains the current brightness level.
+# Changing its value changes the brightness of the screen.
+# maxbrightness contains the maximum possible brightness level.
 
 cd /sys/class/backlight/intel_backlight/
 
 brightnumber=$(< brightness)
 maxbrightnumber=$(< max_brightness)
 step=10
+
+# step is the smallest possible incrememnt/decrement.
+# It's set to 10 by default
 
 if [ "$brightnumber" -ge 11 ] ; then
 brightnumber=$(($brightnumber-$step))
